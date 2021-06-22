cd23295699afd0b257ccd1168b470aa29182f87e
vishal1epam <86233570+vishal1epam@users.noreply.github.com>
2021-06-22 06:57:50 +0530

    updated 1

---
+++ src/Student.java
@@ -0,0 +1,20 @@
+public class Student {
+    private int student_id;
+    private boolean is_Active;
+
+    public int getStudentId() {
+        return student_id;
+    }
+
+    public void setStudentId(int student_id) {
+        this.student_id = student_id;
+    }
+
+    public boolean getIsActive() {
+        return is_Active;
+    }
+
+    public void setIsActive(boolean is_Active) {
+        this.is_Active = is_Active;
+    }
+}
